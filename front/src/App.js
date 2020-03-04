e0ee99d001b57af4c7f13b6dc7ee7cbc87516b60
Willy Cochin <willy.cochin@gmail.com>
2020-03-04 14:05:34 +0100

    init repos

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from "./components/home-page/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path={'/'} component={HomePage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/home-page/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css'
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Willy fait son cirque</h1>
+      </div>
+      <div>
+        <img src="https://media.giphy.com/media/QnuUpgrPgDZEQ/giphy.gif" alt="car"/>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
