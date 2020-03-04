aae66280c42351e5aa98d2cc61e86e1c80f52acc
Willy Cochin <willy.cochin@gmail.com>
2020-03-04 23:03:35 +0100

    Le web c'est génial

---
+++ front/src/components/watcher/Watcher.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from "../global-ui/navbar/NavBar";
+
+const Watcher = () => {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+};
+
+export default Watcher;
